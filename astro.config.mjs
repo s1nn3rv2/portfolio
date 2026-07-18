bdf6389760418f1ebe959733fc222bf8e1402285
s1nn3rv2 <62899437+s1nn3rv2@users.noreply.github.com>
2026-07-18 09:39:15 +0200

    add icon + github pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://s1nn3rv2.github.io',
+  base: '/portfolio',
   vite: {
     plugins: [tailwindcss()]
   },
